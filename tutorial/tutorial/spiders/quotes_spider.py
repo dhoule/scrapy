1196126140cebdc708301dae00e0bdc232c65e8b
dhoule <caine2003@gmail.com>
2019-08-05 13:09:25 -0500

    Updates spiders/quotes_spider.py
    #1 Extracts data from the given URLs; in this case, `text`, `author`,
    and `tags`. Right now, it is just logged in the CLI console.

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -7,18 +7,6 @@ class QuotesSpider(scrapy.Spider):
     # that is, you can't set the same name for different Spiders.
   name = "quotes" 
 
-  # must return an iterable of Requests (you can return a list of 
-    # requests or write a generator function) which the Spider will 
-    # begin to crawl from. Subsequent requests will be generated 
-    # successively from these initial requests.
-  # def start_requests(self):
-  #   urls = [
-  #     'http://quotes.toscrape.com/page/1/',
-  #     'http://quotes.toscrape.com/page/2/',
-  #   ]
-  #   for url in urls:
-  #     yield scrapy.Request(url=url, callback=self.parse)
-
   # Shortcut for implementing start_requests(), is to use start_urls instead.
     # The parse() method will be called to handle each request of the URLs.
     # This is because parse() is Scrapy's default callback method.
@@ -34,10 +22,11 @@ class QuotesSpider(scrapy.Spider):
       # the scraped data as dicts and also finding new URLs to follow 
       # and creating new requests (Request) from them.
   def parse(self, response):
-    page = response.url.split("/")[-2]
-    filename = 'quotes-%s.html' % page
-    with open(filename, 'wb') as f:
-      f.write(response.body)
-    self.log('Saved file %s' % filename)
+    for quote in response.css('div.quote'):
+      yield {
+        'text': quote.css('span.text::text').get(),
+        'author': quote.css('small.author::text').get(),
+        'tags': quote.css('div.tags a.tags::text').getall(),
+      }
 
   
