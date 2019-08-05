e804cdd4962ca92eb60c0f4e3996ef08fb34f389
dhoule <caine2003@gmail.com>
2019-08-05 12:58:31 -0500

    Updates spiders/quotes_spider.py
    Adds comments explaining the code. Comments out the start_requests()
    function to use the short cut `start_urls` array.

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,17 +1,38 @@
+#!/usr/bin/env python3
 import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-  name = "quotes"
+  # `name` identifies the Spider. It must be unique within a project, 
+    # that is, you can't set the same name for different Spiders.
+  name = "quotes" 
 
-  def start_requests(self):
-    urls = [
+  # must return an iterable of Requests (you can return a list of 
+    # requests or write a generator function) which the Spider will 
+    # begin to crawl from. Subsequent requests will be generated 
+    # successively from these initial requests.
+  # def start_requests(self):
+  #   urls = [
+  #     'http://quotes.toscrape.com/page/1/',
+  #     'http://quotes.toscrape.com/page/2/',
+  #   ]
+  #   for url in urls:
+  #     yield scrapy.Request(url=url, callback=self.parse)
+
+  # Shortcut for implementing start_requests(), is to use start_urls instead.
+    # The parse() method will be called to handle each request of the URLs.
+    # This is because parse() is Scrapy's default callback method.
+  start_urls = [
       'http://quotes.toscrape.com/page/1/',
       'http://quotes.toscrape.com/page/2/',
     ]
-    for url in urls:
-      yield scrapy.Request(url=url, callback=self.parse)
-
+  # method that will be called to handle the response downloaded for 
+    # each of the requests made. The response parameter is an instance 
+    # of TextResponse that holds the page content and has further 
+    # helpful methods to handle it.
+    # NOTE: The parse() method usually parses the response, extracting 
+      # the scraped data as dicts and also finding new URLs to follow 
+      # and creating new requests (Request) from them.
   def parse(self, response):
     page = response.url.split("/")[-2]
     filename = 'quotes-%s.html' % page
@@ -19,4 +40,4 @@ class QuotesSpider(scrapy.Spider):
       f.write(response.body)
     self.log('Saved file %s' % filename)
 
-# Current stopping point: https://doc.scrapy.org/en/latest/intro/tutorial.html#our-first-spider
+  
